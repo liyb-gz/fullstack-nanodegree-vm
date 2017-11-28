18ee520d81dfad37c58122febd6d5cca8c88e62b
Felix Li <liyb.gz@gmail.com>
2017-11-28 22:08:22 +0800

    Add item class

--- vagrant/catalog/models.py
+++ vagrant/catalog/models.py
@@ -33,6 +33,30 @@ class Category(Base):
 			'description': self.description,
 			'creator_id': self.creator_id
 		}
- 
+
+class Item(Base):
+	__tablename__ = 'item'
+
+	id = Column(Integer, primary_key = True)
+	name = Column(String(60), index = True)
+	description = Column(String(5000))
+
+	category_id = Column(Integer, ForeignKey('category.id'), nullable = False)
+	category = relationship(Category)
+
+	# TODO: add nullable = False to creator id after adding authentication
+	creator_id = Column(Integer, ForeignKey('user.id'))
+	creator = relationship(User)
+
+	@property
+	def serialize(self):
+		return {
+			'id': self.id,
+			'name': self.name,
+			'description': self.description,
+			'category_id': self.category_id,
+			'creator_id': self.creator_id
+		}
+
 engine = create_engine('sqlite:///catalog.db')
 Base.metadata.create_all(engine)

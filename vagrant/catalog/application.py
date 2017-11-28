dc762910c55ebcc7a18325c91ced1cd0dca8ae8f
Felix Li <liyb.gz@gmail.com>
2017-11-28 22:39:28 +0800

    Change index entry to accept GET and POST

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,4 +1,4 @@
-from models import User, Base
+from models import User, Base, Category, Item
 from flask import Flask, jsonify, request, url_for, abort, g
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
@@ -7,13 +7,24 @@ from sqlalchemy import create_engine
 engine = create_engine('sqlite:///catalog.db')
 
 Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
+DBSession = sessionmaker(bind = engine)
 session = DBSession()
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return 'Hello'
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/categories/', methods = ['GET', 'POST'])
+def showAllCategories():
+	""" Homepage. 
+	GET: Show all categories. 
+	POST: Create a new category."""
+	if request.method == 'GET':
+		return 'Show all categories. '
+	elif request.method == 'POST':
+		return createCategory()
+
+def createCategory():
+	return 'Create new category'
+
 
 if __name__ == '__main__':
     app.debug = True
